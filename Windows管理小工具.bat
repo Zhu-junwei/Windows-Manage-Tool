804b332f9794871df0b45180a423691a7b8c8fce
今天幸福不在家 <zhujwmail@163.com>
2025-08-08 18:40:14 +0800

    精简资源管理器选项；增加导航栏控制面板开关

--- Windows管理小工具.bat
+++ Windows管理小工具.bat
@@ -558,80 +558,156 @@ exit /b
 call :print_title "资源管理器设置"
 set "a=" 
 call :print_separator
-echo			1. 默认打开 此电脑         11. 禁用 U盘 &echo.
-echo			2. 默认打开 主文件夹       12. 启用 U盘 &echo.
-echo			3. 显示 扩展(后缀)名       13. Windows 10此电脑文件夹设置 &echo.
-echo			4. 隐藏 扩展(后缀)名       14. 主文件夹开关&echo.
-echo			5. 单击 打开文件           15. 图库开关&echo.
-echo			6. 双击 打开文件           16. 清理图标/缩略图缓存 &echo.
-echo			7. 显示 复选框 &echo.
-echo			8. 隐藏 复选框 &echo.
-echo			9. 显示 系统隐藏文件 &echo.
-echo			10. 隐藏 系统隐藏文件 &echo.
-echo			0. 返回(q) &echo.
+echo			 1. 默认打开[此电脑/主文件夹]         11. Windows 10此电脑文件夹设置 &echo.
+echo			 2. 文件扩展(后缀)名开关 &echo.
+echo			 3. [单击/双击]打开文件 &echo.
+echo			 4. [显示/隐藏]复选框 &echo.
+echo			 5. [显示/隐藏]系统隐藏文件 &echo.
+echo			 6. U盘禁用开关 &echo.
+echo			 7. 导航栏-主文件夹开关 &echo.
+echo			 8. 导航栏-图库开关 &echo.
+echo			 9. 导航栏-控制面板开关 &echo.
+echo			10. 清理图标/缩略图缓存 &echo.
+echo			 0. 返回(q) &echo.
 call :print_separator
-set /p a=请输入你的选择: 
+set /p "a=请输入你的选择:"
 if "%a%"=="1" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f >nul 2>&1
-	call :sleep "已设置默认打开此电脑！" 5
+	echo 正在执行默认打开此电脑主文件夹设置...
+	choice /c 12 /n /m "请选择你的操作? [1.主文件夹(系统默认) 2.此电脑]:"
+	if errorlevel 2 (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f >nul 2>&1
+		call :sleep "已设置默认打开此电脑！" 5
+	) else (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 0 /f >nul 2>&1
+		call :sleep "已设置默认打开主文件夹！" 5
+	)
 ) else if "%a%"=="2" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 0 /f >nul 2>&1
-	call :sleep "已设置默认打开主文件夹！" 5
+	echo.&echo 文件扩展（后缀）名设置 
+	choice /c 12 /n /m "请选择你的操作? [1.显示 2.隐藏（系统默认）]: "
+	if errorlevel 2 (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 1 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已隐藏扩展名" 6
+	) else (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已显示扩展名" 6
+	)
 ) else if "%a%"=="3" (
-	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
-	call :restart_explorer
-	call :sleep "已显示扩展名" 6
+	echo.&echo [单击/双击]打开文件设置 
+	choice /c 12 /n /m "请选择你的操作? [1.单击 2.双击（系统默认）]: "
+	if errorlevel 2 (
+		reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V ShellState /T REG_BINARY /D 240000003ea8000000000000000000000000000001000000130000000000000062000000 /F >nul 2>&1
+		call :restart_explorer
+		call :sleep "已设置双击打开文件！" 3
+	) else (
+		reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V IconUnderline /T REG_DWORD /D 2 /F >nul 2>&1
+		reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V ShellState /T REG_BINARY /D 240000001ea8000000000000000000000000000001000000130000000000000062000000 /F >nul 2>&1
+		call :restart_explorer
+		call :sleep "已设置单击打开文件！" 3
+	)
 ) else if "%a%"=="4" (
-	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 1 /f
-	call :restart_explorer
-	call :sleep "已隐藏扩展名" 6
+	echo.&echo [显示/隐藏]复选框 
+	choice /c 12 /n /m "请选择你的操作? [1.显示 2.隐藏（系统默认）]: "
+	if errorlevel 2 (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AutoCheckSelect" /t REG_DWORD /d 0 /f >nul 2>&1
+		call :sleep "已隐藏复选框，手动刷新生效" 6
+	) else (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AutoCheckSelect" /t REG_DWORD /d 1 /f >nul 2>&1
+		call :sleep "已显示复选框，手动刷新生效" 6
+	)
 ) else if "%a%"=="5" (
-	REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V IconUnderline /T REG_DWORD /D 2 /F >nul 2>&1
-	REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V ShellState /T REG_BINARY /D 240000001ea8000000000000000000000000000001000000130000000000000062000000 /F >nul 2>&1
-	call :restart_explorer
-	call :sleep "已设置单击打开文件！" 3
+	echo.&echo [显示/隐藏]系统隐藏文件 
+	choice /c 12 /n /m "请选择你的操作? [1.显示 2.隐藏（系统默认）]: "
+	if errorlevel 2 (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 2 /f >nul 2>&1
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 0 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已隐藏系统隐藏文件，正在重启资源管理器" 6
+	) else (
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f >nul 2>&1
+		reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已显示系统隐藏文件，正在重启资源管理器" 6
+	)
 ) else if "%a%"=="6" (
-	REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V ShellState /T REG_BINARY /D 240000003ea8000000000000000000000000000001000000130000000000000062000000 /F >nul 2>&1
-	call :restart_explorer
-	call :sleep "已设置双击打开文件！" 3
+	echo.echo 开启或禁用插入U盘 
+	choice /c 12 /n /m "请选择你的操作? [1.启用（系统默认） 2.禁用]:"
+	if errorlevel 2 (
+		reg add "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d 4 /f >nul 2>&1
+		call :sleep "已禁用U盘使用" 6
+	) else (
+		reg add "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d 3 /f >nul 2>&1
+		call :sleep "已启用U盘使用" 6
+	)
 ) else if "%a%"=="7" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AutoCheckSelect" /t REG_DWORD /d 1 /f >nul 2>&1
-	call :sleep "已显示复选框，手动刷新生效" 6
-) else if "%a%"=="8" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "AutoCheckSelect" /t REG_DWORD /d 0 /f >nul 2>&1
-	call :sleep "已隐藏复选框，手动刷新生效" 6
-) else if "%a%"=="9" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f
-	call :restart_explorer
-	call :sleep "已显示系统隐藏文件，正在重启资源管理器" 6
-) else if "%a%"=="10" (
-	reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 2 /f >nul 2>&1
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 0 /f >nul 2>&1
-	call :restart_explorer
-	call :sleep "已隐藏系统隐藏文件，正在重启资源管理器" 6
-) else if "%a%"=="11" (
-	call :usbstor_setting 4
-	call :sleep "已禁用U盘使用" 6
-) else if "%a%"=="12" (
-	call :usbstor_setting 3
-	call :sleep "已启用U盘使用" 6
-) else if "%a%"=="13" (
-	call :this_computer_folder
-) else if "%a%"=="14" (
 	call :explorer_home_folder_toggle
-) else if "%a%"=="15" (
+) else if "%a%"=="8" (
 	call :explorer_gallery_toggle
-) else if "%a%"=="16" (
+) else if "%a%"=="9" (
+	choice /c 12 /n /m "控制面板设置? [1.隐藏（系统默认） 2.显示]: "
+	if errorlevel 2 (
+		reg add "HKCU\Software\Classes\CLSID\{26EE0668-A00A-44D7-9371-BEB064C98683}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 1 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已显示控制面板" 6
+	) else (
+		reg add "HKCU\Software\Classes\CLSID\{26EE0668-A00A-44D7-9371-BEB064C98683}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f >nul 2>&1
+		call :restart_explorer
+		call :sleep "已隐藏控制面板" 6
+	)
+) else if "%a%"=="10" (
 	call :clear_icon_cache
+) else if "%a%"=="11" (
+	call :this_computer_folder
 )
 if "%a%"=="0" exit /b
 if /i "%a%"=="q" exit /b
 goto :explorer_setting
 
-:: U盘设置 4:禁用 3:启用
-:usbstor_setting
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d %~1 /f
+:: 资源管理器-导航栏-主文件夹 
+:explorer_home_folder_toggle
+set "HOME_FOLDER_TOGGLE_REG=tmp.reg"
+choice /c 123 /n /m "主文件夹设置? [1.隐藏 2.显示 3.返回]: "
+if "%errorlevel%"=="3" exit /b
+if "%errorlevel%"=="1" (set "v=0" & set "op_name=隐藏") else (set "v=1" & set "op_name=显示")
+(
+	echo Windows Registry Editor Version 5.00
+	echo.
+	echo [HKEY_CURRENT_USER\Software\Classes\CLSID\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}]
+	echo "System.IsPinnedToNameSpaceTree"=dword:0000000%v%
+) > "%HOME_FOLDER_TOGGLE_REG%"
+regedit.exe /s "%HOME_FOLDER_TOGGLE_REG%"
+IF EXIST "%HOME_FOLDER_TOGGLE_REG%" DEL "%HOME_FOLDER_TOGGLE_REG%"
+call :sleep "已设置主文件夹 %op_name% ，请重新打开资源管理器查看效果" 10
+exit /b
+
+:: 资源管理器设置-导航栏-图库 
+:explorer_gallery_toggle
+set "GALLERY_TOGGLE_REG=tmp.reg"
+choice /c 123 /n /m "图库设置? [1.隐藏 2.显示 3.返回]: "
+if "%errorlevel%"=="3" exit /b
+if "%errorlevel%"=="1" (set "v=0" & set "op_name=隐藏") else (set "v=1" & set "op_name=显示")
+(
+	echo Windows Registry Editor Version 5.00
+	echo.
+	echo [HKEY_CURRENT_USER\Software\Classes\CLSID\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}]
+	echo "System.IsPinnedToNameSpaceTree"=dword:0000000%v%
+) > "%GALLERY_TOGGLE_REG%"
+regedit.exe /s "%GALLERY_TOGGLE_REG%"
+IF EXIST "%GALLERY_TOGGLE_REG%" DEL "%GALLERY_TOGGLE_REG%"
+call :sleep "已设置图库 %op_name% ，请重新打开资源管理器查看效果" 10
+exit /b
+
+:: 清理图标/缩略图缓存 
+:clear_icon_cache
+taskkill /f /im explorer.exe >nul 2>&1
+echo 正在删除图标缓存...
+del /f /s /q "%localappdata%\IconCache.db" >nul 2>&1
+del /f /s /q "%localappdata%\Microsoft\Windows\Explorer\iconcache*" >nul 2>&1
+echo 正在清理缩略图缓存...
+del /f /s /q "%localappdata%\Microsoft\Windows\Explorer\thumbcache_*" >nul 2>&1
+call :restart_explorer
+call :sleep "清理完成" 6
 exit /b
 
 :: Windows 10 此电脑文件夹管理
@@ -743,52 +819,6 @@ if "%b%"=="1" (
 if "%b%"=="0" exit /b
 if /i "%b%"=="q" exit /b
 goto this_computer_folder
-
-:: 资源管理器设置-左侧【主文件夹】显示隐藏 
-:explorer_home_folder_toggle
-set "HOME_FOLDER_TOGGLE_REG=tmp.reg"
-choice /c 123 /n /m "主文件夹设置? [1.隐藏 2.显示 3.返回]: "
-if "%errorlevel%"=="3" exit /b
-if "%errorlevel%"=="1" (set "v=0" & set "op_name=隐藏") else (set "v=1" & set "op_name=显示")
-(
-	echo Windows Registry Editor Version 5.00
-	echo.
-	echo [HKEY_CURRENT_USER\Software\Classes\CLSID\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}]
-	echo "System.IsPinnedToNameSpaceTree"=dword:0000000%v%
-) > "%HOME_FOLDER_TOGGLE_REG%"
-regedit.exe /s "%HOME_FOLDER_TOGGLE_REG%"
-IF EXIST "%HOME_FOLDER_TOGGLE_REG%" DEL "%HOME_FOLDER_TOGGLE_REG%"
-call :sleep "已设置主文件夹 %op_name% ，请重新打开资源管理器查看效果" 10
-exit /b
-
-:: 资源管理器设置-左侧【图库】显示隐藏 
-:explorer_gallery_toggle
-set "GALLERY_TOGGLE_REG=tmp.reg"
-choice /c 123 /n /m "图库设置? [1.隐藏 2.显示 3.返回]: "
-if "%errorlevel%"=="3" exit /b
-if "%errorlevel%"=="1" (set "v=0" & set "op_name=隐藏") else (set "v=1" & set "op_name=显示")
-(
-	echo Windows Registry Editor Version 5.00
-	echo.
-	echo [HKEY_CURRENT_USER\Software\Classes\CLSID\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}]
-	echo "System.IsPinnedToNameSpaceTree"=dword:0000000%v%
-) > "%GALLERY_TOGGLE_REG%"
-regedit.exe /s "%GALLERY_TOGGLE_REG%"
-IF EXIST "%GALLERY_TOGGLE_REG%" DEL "%GALLERY_TOGGLE_REG%"
-call :sleep "已设置图库 %op_name% ，请重新打开资源管理器查看效果" 10
-exit /b
-
-:: 清理图标/缩略图缓存 
-:clear_icon_cache
-taskkill /f /im explorer.exe >nul 2>&1
-echo 正在删除图标缓存...
-del /f /s /q "%localappdata%\IconCache.db" >nul 2>&1
-del /f /s /q "%localappdata%\Microsoft\Windows\Explorer\iconcache*" >nul 2>&1
-echo 正在清理缩略图缓存...
-del /f /s /q "%localappdata%\Microsoft\Windows\Explorer\thumbcache_*" >nul 2>&1
-call :restart_explorer
-call :sleep "清理完成" 6
-exit /b
 
 :: 下载 Windows
 :download_windows_office
@@ -964,7 +994,7 @@ if "%a%"=="1" (
 		if "!errorlevel!" == "1" (shutdown /r /t 0)
 	)
 ) else if "%a%"=="4" (
-	UserAccountControlSettings.exe
+	start "" UserAccountControlSettings.exe
 )
 if "%a%"=="0" exit /b
 if /i "%a%"=="q" exit /b
@@ -1804,8 +1834,7 @@ if not exist "%BAT_NEW_TMP%" (
 )
 set "MAIN_SCRIPT_PATH=%~f0"
 (
-    echo @echo off
-    echo chcp 65001^>nul
+    echo @echo off ^& chcp 65001^>nul
     echo copy /Y "%BAT_NEW_TMP%" "%MAIN_SCRIPT_PATH%"
     echo del /F /Q "%BAT_NEW_TMP%"
     echo start "" "%MAIN_SCRIPT_PATH%"
